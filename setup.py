63e9a481c87e3301528ce6d5f4d65f91cb3bd4b0
Adina Wagner <adina.wagner@t-online.de>
2020-06-09 10:46:34 +0200

    modernize setup according to https://github.com/datalad/datalad-ukbiobank

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
-from setuptools import find_packages
+import versioneer
 
-setup(
-    # basic project properties can be set arbitrarily
-    name="datalad_helloworld",
-    author="The DataLad Team and Contributors",
-    author_email="team@datalad.org",
-    version='0.1',
-    description="demo DataLad extension package",
-    packages=[pkg for pkg in find_packages('.') if pkg.startswith('datalad')],
-    # datalad command suite specs from here
-    install_requires=[
-        # in general datalad will be a requirement, unless the datalad extension
-        # aspect is an optional component of a larger project
-        # disable for now as we currently need a Git snapshot (requirements.txt)
-        #'datalad',
-    ],
-    entry_points = {
-        # 'datalad.extensions' is THE entrypoint inspected by the datalad API builders
-        'datalad.extensions': [
-            # the label in front of '=' is the command suite label
-            # the entrypoint can point to any symbol of any name, as long it is
-            # valid datalad interface specification (see demo in this extensions
-            'helloworld=datalad_helloworld:command_suite',
-        ]
-    },
-)
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# Should match pyproject.toml
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
+
+if __name__ == '__main__':
+    setup(name='datalad_helloworld',
+          version=versioneer.get_version(),
+          author="The DataLad Team and Contributors",
+          author_email="team@datalad.org",
+          description="demo DataLad extension package",
+          cmdclass=versioneer.get_cmdclass(),
+          setup_requires=SETUP_REQUIRES,
+          entry_points={
+              # 'datalad.extensions' is THE entrypoint inspected by the datalad API builders
+              'datalad.extensions': [
+                  # the label in front of '=' is the command suite label
+                  # the entrypoint can point to any symbol of any name, as long it is
+                  # valid datalad interface specification (see demo in this extensions
+                  'helloworld=datalad_helloworld:command_suite',
+              ],
+          },
+    )
+
