5d0f6ba43dcaf2db1903867783f3fd58f9ece983
Benjamin Poldrack <bpoldrack@users.noreply.github.com>
2022-05-30 13:17:15 +0200

    Initial commit

---
+++ datalad_helloworld/__init__.py
@@ -0,0 +1,34 @@
+"""DataLad demo extension"""
+
+__docformat__ = 'restructuredtext'
+
+import logging
+lgr = logging.getLogger('datalad.helloworld')
+
+# Defines a datalad command suite.
+# This variable must be bound as a setuptools entrypoint
+# to be found by datalad
+command_suite = (
+    # description of the command suite, displayed in cmdline help
+    "Demo DataLad command suite",
+    [
+        # specification of a command, any number of commands can be defined
+        (
+            # importable module that contains the command implementation
+            'datalad_helloworld.hello_cmd',
+            # name of the command class implementation in above module
+            'HelloWorld',
+            # optional name of the command in the cmdline API
+            'hello-cmd',
+            # optional name of the command in the Python API
+            'hello_cmd'
+        ),
+    ]
+)
+
+from datalad import setup_package
+from datalad import teardown_package
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+import versioneer
+
+from _datalad_buildsupport.setup import (
+    BuildManPage,
+)
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass.update(build_manpage=BuildManPage)
+
+if __name__ == '__main__':
+    setup(name='datalad_helloworld',
+          version=versioneer.get_version(),
+          cmdclass=cmdclass,
+    )
