290dad70b9d444c389d8d9211dae8e52d487fe2e
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2022-06-01 11:15:23 +0200

    Basic adjustements of extension template

--- datalad_dataverse/__init__.py
+++ datalad_dataverse/__init__.py
@@ -1,21 +1,21 @@
-"""DataLad demo extension"""
+"""DataLad Dataverse extension"""
 
 __docformat__ = 'restructuredtext'
 
 import logging
-lgr = logging.getLogger('datalad.helloworld')
+lgr = logging.getLogger('datalad.dataverse')
 
 # Defines a datalad command suite.
 # This variable must be bound as a setuptools entrypoint
 # to be found by datalad
 command_suite = (
     # description of the command suite, displayed in cmdline help
-    "Demo DataLad command suite",
+    "DataLad Dataverse command suite",
     [
         # specification of a command, any number of commands can be defined
         (
             # importable module that contains the command implementation
-            'datalad_helloworld.hello_cmd',
+            'datalad_dataverse.hello_cmd',
             # name of the command class implementation in above module
             'HelloWorld',
             # optional name of the command in the cmdline API
