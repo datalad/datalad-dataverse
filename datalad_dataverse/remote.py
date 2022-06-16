57919711a28afb10f33b4365138020198205bcc1
Michael Hanke <michael.hanke@gmail.com>
2022-06-16 11:40:10 +0100

    Disfunctional special remote scaffold

---
+++ datalad_dataverse/remote.py
@@ -0,0 +1,37 @@
+from datalad.customremotes import SpecialRemote
+from datalad.customremotes.main import main as super_main
+
+
+class DataverseRemote(SpecialRemote):
+    def initremote(self):
+        raise
+
+    def prepare(self):
+        raise
+        pass
+
+    def checkpresent(self, key):
+        raise
+        pass
+
+    def transfer_store(self, key, local_file):
+        raise
+        pass
+
+    def transfer_retrieve(self, key, file):
+        raise
+        pass
+
+    def remove(self, key):
+        raise
+        pass
+
+
+def main():
+    """cmdline entry point"""
+    super_main(
+        cls=SpecialRemote,
+        remote_name='dataverse',
+        description=\
+        "transport file content to and from a Dataverse dataset",
+)
