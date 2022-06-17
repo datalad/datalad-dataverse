1077af5a5654d070daefa503ba564ce491671d2b
Michael Hanke <michael.hanke@gmail.com>
2022-06-17 17:21:39 +0100

    Add a test to show that git-push|clone also works with dataverse

--- datalad_dataverse/tests/test_remote.py
+++ datalad_dataverse/tests/test_remote.py
@@ -1,6 +1,10 @@
 from unittest.mock import patch
+from urllib.parse import quote as urlquote
 
-from datalad.api import Dataset
+from datalad.api import (
+    Dataset,
+    clone,
+)
 from datalad.tests.utils_pytest import (
     skip_if,
     with_tempfile,
@@ -63,3 +67,37 @@ def _check_remote(ds, dspid):
     ds.repo.call_annex([
         'testremote', '--fast', 'mydv',
     ])
+
+
+@skip_if(cond='testadmin' not in API_TOKENS)
+@with_tempfile
+@with_tempfile
+def test_datalad_annex(dspath=None, clonepath=None):
+    ds = Dataset(dspath).create()
+    admin_api = get_native_api(DATAVERSE_URL, API_TOKENS['testadmin'])
+    create_test_dataverse_collection(admin_api, 'basetest')
+    dspid = create_test_dataverse_dataset(admin_api, 'basetest', 'testds')
+    try:
+        with patch.dict('os.environ', {
+                'DATAVERSE_API_TOKEN': API_TOKENS['testadmin']}):
+            _check_datalad_annex(ds, dspid, clonepath)
+
+    finally:
+        admin_api.destroy_dataset(dspid)
+
+
+def _check_datalad_annex(ds, dspid, clonepath):
+    repo = ds.repo
+    # this is the raw datalad-annex URL, convenience could be added on top
+    git_remote_url = 'datalad-annex::?type=external&externaltype=dataverse&' \
+                     f'url={urlquote(DATAVERSE_URL)}&doi={urlquote(dspid)}&' \
+                     'encryption=none'
+
+    repo.call_git(['remote', 'add', 'mydv', git_remote_url])
+    repo.call_git(['push', 'mydv', '--all'])
+
+    dsclone = clone(git_remote_url, clonepath)
+
+    # we got the same thing
+    assert repo.get_hexsha(ds.repo.get_corresponding_branch()) == \
+        dsclone.repo.get_hexsha(ds.repo.get_corresponding_branch())
