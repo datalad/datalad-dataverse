859abc3610f59666248dbb9bb3399d5acad99ceb
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2022-06-18 14:43:39 +0200

    TST: Enhance basic test

--- datalad_dataverse/tests/test_create_sibling_dataverse.py
+++ datalad_dataverse/tests/test_create_sibling_dataverse.py
@@ -1,12 +1,14 @@
 from unittest.mock import patch
 from requests.exceptions import ConnectionError
 from datalad.tests.utils_pytest import (
+    assert_in,
     assert_raises,
     assert_result_count,
     skip_if,
     with_tempfile,
 )
 from datalad.api import (
+    clone,
     create_sibling_dataverse,
 )
 from datalad.distribution.dataset import (
@@ -26,16 +28,17 @@ from datalad_dataverse.utils import get_native_api
 
 @skip_if(cond='testadmin' not in API_TOKENS)
 @with_tempfile
-def test_basic(path=None):
+@with_tempfile
+def test_basic(path=None, clone_path=None):
     ds = Dataset(path).create()
     (ds.pathobj / 'somefile.txt').write_text('content')
     ds.save()
     admin_api = get_native_api(DATAVERSE_URL, API_TOKENS['testadmin'])
     create_test_dataverse_collection(admin_api, 'basetest')
-    _check_basic_creation(ds, 'basetest', 'testadmin')
+    _check_basic_creation(ds, 'basetest', 'testadmin', clone_path)
 
 
-def _check_basic_creation(ds, collection_alias, user):
+def _check_basic_creation(ds, collection_alias, user, clone_path):
 
     with patch.dict('os.environ', {
             'DATALAD_CREDENTIAL_TESTCRED_TOKEN': API_TOKENS[user]}):
@@ -51,3 +54,26 @@ def _check_basic_creation(ds, collection_alias, user):
                                               recursion_limit=None,
                                               metadata=None)
         assert_result_count(results, 0, status='error')
+        assert_result_count(results, 1,
+                            status='ok',
+                            action='create_sibling_dataverse')
+        assert_result_count(results, 1,
+                            status='ok',
+                            action='create_sibling_dataverse.storage')
+        assert_result_count(results, 2)
+
+        assert_in('doi', results[0])
+        assert_in('doi', results[1])
+
+        clone_url = [r['url'] for r in results
+                     if r['action'] == "create_sibling_dataverse"][0]
+
+        # push should work now
+        ds.push(to="git_remote")
+        # drop content and retrieve again
+        ds.drop("somefile.txt")
+        ds.get("somefile.txt")
+
+        # And we should be able to clone
+        clone = clone(source=clone_url, path=clone_path)
+        clone.get("somefile.txt")
