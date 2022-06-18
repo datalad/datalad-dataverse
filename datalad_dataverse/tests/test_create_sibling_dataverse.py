a065ad91576e1bb5ef44a48840e8e3dd7d901e57
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2022-06-18 11:16:37 +0200

    TST: Start most basic test

--- datalad_dataverse/tests/test_create_sibling_dataverse.py
+++ datalad_dataverse/tests/test_create_sibling_dataverse.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from requests.exceptions import ConnectionError
 from datalad.tests.utils_pytest import (
     assert_raises,
@@ -17,17 +18,36 @@ from datalad_dataverse.tests import (
     API_TOKENS,
     DATAVERSE_URL
 )
+from datalad_dataverse.tests.utils import (
+    create_test_dataverse_collection,
+)
+from datalad_dataverse.utils import get_native_api
 
 
-@skip_if(cond=True)  # condition should become `not DATAVERSE_URL` or alike
+@skip_if(cond='testadmin' not in API_TOKENS)
 @with_tempfile
-def test_dummy(path=None):
-
-    # This test is nonsense! For now just proves that create-sibing can make a
-    # connection to the CI dataverse
-
+def test_basic(path=None):
     ds = Dataset(path).create()
+    (ds.pathobj / 'somefile.txt').write_text('content')
+    ds.save()
+    admin_api = get_native_api(DATAVERSE_URL, API_TOKENS['testadmin'])
+    create_test_dataverse_collection(admin_api, 'basetest')
+    _check_basic_creation(ds, 'basetest', 'testadmin')
 
-    assert_raises(ConnectionError, ds.create_sibling_dataverse,
-                  url="http://doesnot-exi.st", existing="skip")
-    ds.create_sibling_dataverse(url="http://localhost:8080", existing="skip")
+
+def _check_basic_creation(ds, collection_alias, user):
+
+    with patch.dict('os.environ', {
+            'DATALAD_CREDENTIAL_TESTCRED_TOKEN': API_TOKENS[user]}):
+
+        results = ds.create_sibling_dataverse(url=DATAVERSE_URL,
+                                              collection=collection_alias,
+                                              name='git_remote',
+                                              storage_name='special_remote',
+                                              mode='annex',
+                                              credential='testcred',
+                                              existing='error',
+                                              recursive=False,
+                                              recursion_limit=None,
+                                              metadata=None)
+        assert_result_count(results, 0, status='error')
