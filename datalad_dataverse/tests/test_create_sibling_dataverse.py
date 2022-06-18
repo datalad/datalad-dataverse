9ab6c3c756c10aa0ef7b8d226b96809f8700886e
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2022-06-18 16:47:37 +0200

    Add basic test for export creation

--- datalad_dataverse/tests/test_create_sibling_dataverse.py
+++ datalad_dataverse/tests/test_create_sibling_dataverse.py
@@ -85,3 +85,64 @@ def _check_basic_creation(ds, collection_alias, user, clone_path):
                           result_xfm='datasets')
         cloned_ds.repo.enable_remote('special_remote')
         cloned_ds.get("somefile.txt")
+
+
+@skip_if(cond='testadmin' not in API_TOKENS)
+@with_tempfile
+@with_tempfile
+def test_basic_export(path=None, clone_path=None):
+    ds = Dataset(path).create()
+    (ds.pathobj / 'somefile.txt').write_text('content')
+    ds.save()
+    admin_api = get_native_api(DATAVERSE_URL, API_TOKENS['testadmin'])
+    create_test_dataverse_collection(admin_api, 'basetest')
+    _check_basic_creation(ds, 'basetest', 'testadmin', clone_path)
+
+
+@with_credential(
+    'dataverse',
+    secret=API_TOKENS.get('testadmin'),
+    realm=f'{DATAVERSE_URL.rstrip("/")}/dataverse',
+)
+def _check_basic_export_creation(ds, collection_alias, user, clone_path):
+
+    with patch.dict('os.environ', {
+            'DATALAD_CREDENTIAL_TESTCRED_TOKEN': API_TOKENS[user],
+            'DATALAD_CREDENTIAL_TESTCRED_REALM': DATAVERSE_URL}):
+
+        results = ds.create_sibling_dataverse(url=DATAVERSE_URL,
+                                              collection=collection_alias,
+                                              name='git_remote',
+                                              storage_name='special_remote',
+                                              mode='filetree',
+                                              credential='testcred',
+                                              existing='error',
+                                              recursive=False,
+                                              recursion_limit=None,
+                                              metadata=None)
+        assert_result_count(results, 0, status='error')
+        assert_result_count(results, 1,
+                            status='ok',
+                            action='create_sibling_dataverse')
+        assert_result_count(results, 1,
+                            status='ok',
+                            action='create_sibling_dataverse.storage')
+        assert_result_count(results, 2)
+
+        assert_in('doi', results[0])
+        assert_in('doi', results[1])
+
+        clone_url = [r['url'] for r in results
+                     if r['action'] == "create_sibling_dataverse"][0]
+
+        # push should work now
+        ds.push(to="git_remote")
+        # drop content and retrieve again
+        ds.drop("somefile.txt")
+        ds.get("somefile.txt")
+
+        # And we should be able to clone
+        cloned_ds = clone(source=clone_url, path=clone_path,
+                          result_xfm='datasets')
+        cloned_ds.repo.enable_remote('special_remote')
+        cloned_ds.get("somefile.txt")
