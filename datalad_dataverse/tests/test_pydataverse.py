3251cf2e7f8da0be914293a986e2c8876509ff52
Adina Wagner <adina.wagner@t-online.de>
2024-08-23 09:53:23 +0200

    Use dataverse_instance_url fixture

--- datalad_dataverse/tests/test_pydataverse.py
+++ datalad_dataverse/tests/test_pydataverse.py
@@ -2,7 +2,6 @@
 
 import datetime
 import json
-import os
 from requests import delete
 from requests.auth import HTTPBasicAuth
 
@@ -26,6 +25,7 @@ def test_file_handling(
         dataverse_admin_api,
         dataverse_dataaccess_api,
         dataverse_dataset,
+        dataverse_instance_url,
 ):
     # the starting point of `dataverse_dataset` is a freshly
     # created, non-published dataset in draft mode, with no prior
@@ -42,7 +42,7 @@ def test_file_handling(
 
     fileid = check_upload(
         dataverse_admin_api,
-        dataverse_dataset, fcontent, fpath, src_md5)
+        dataverse_dataset, fcontent, fpath, src_md5, dataverse_instance_url)
 
     check_download(
         dataverse_dataaccess_api, fileid,
@@ -105,7 +105,7 @@ def check_duplicate_file_deposition(api, dsid, tmp_path):
                for f in identicals)
 
 
-def check_upload(api, dsid, fcontent, fpath, src_md5):
+def check_upload(api, dsid, fcontent, fpath, src_md5, dv_url):
     # the simplest possible upload, just a source file name
     response = api.upload_datafile(
         identifier=dsid,
@@ -142,7 +142,6 @@ def check_upload(api, dsid, fcontent, fpath, src_md5):
     #assert df['pidURL'] == ''
     assert df['rootDataFileId'] == -1
 
-    dv_url = os.getenv('DATAVERSE_TEST_BASEURL')
     if 'localhost' in dv_url or '127.0.0.1' in dv_url:
         assert df['storageIdentifier'].startswith('local://')
     else:
